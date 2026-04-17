666ec5281463ef126a7f8eb6d8d2647777c58160
Allyson Jeronimo <allysonjeronimo@gmail.com>
2026-04-16 21:30:13 -0300

    Bomb scene

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -10,13 +10,11 @@ func show_message(message):
 	$MessageLabel.show()
 	
 func show_game_over():
-	show_message("Game Over!")
+	show_message("Game Over")
 	await get_tree().create_timer(2.0).timeout
-	
-	show_message("Dodge The Creeps")
+	show_message("Dodge The Creeps!")
 	$StartButton.show()
-	
-	
+		
 func _on_start_button_pressed() -> void:
 	start.emit()
 	$StartButton.hide()
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,21 +2,27 @@ extends Node2D
 
 var score
 @export var enemy_scene: PackedScene
+@export var bomb_scene: PackedScene
 
 func _ready():
 	pass
 	
 func start_game():
 	score = 0
+	$HUD.update_score(score)
+	get_tree().call_group("enemy", "queue_free")
 	$ScoreTimer.start()
 	$EnemyTimer.start()
 	$Player.start($StartPosition.position)
+	$BackMusic.play()
 	
 func game_over():
 	$Player.hide()
 	$EnemyTimer.stop()
 	$ScoreTimer.stop()
 	$HUD.show_game_over()
+	$BackMusic.stop()
+	$DeathSound.play()
 	
 func _on_enemy_timer_timeout() -> void:
 	var new_enemy = enemy_scene.instantiate()
@@ -40,6 +46,11 @@ func _on_enemy_timer_timeout() -> void:
 func _on_score_timer_timeout() -> void:
 	score += 1
 	$HUD.update_score(score)
+	if(score % 3 == 0):
+		spawn_bomb()
+	
+func spawn_bomb():
+	print("Spawn Bomb!")
 
 func _on_player_hit() -> void:
 	game_over()
